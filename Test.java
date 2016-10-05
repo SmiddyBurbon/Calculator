8fac49b140eaea4554a85063e649439acabb0cc4
desch296 <me@dennisschmidt.net>
2016-10-05 17:22:27 +0200

    added user input

--- Test.java
+++ Test.java
@@ -3,5 +3,10 @@ import java.util.*;
 public class Test {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Bitte eingeben [+-*/] zahl1 zahl2:");
+		String operator = scanner.next();
+		int zahl1 = scanner.nextInt();
+		int zahl2 = scanner.nextInt();
 	}
 }
