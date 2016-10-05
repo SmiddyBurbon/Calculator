2c318d42b51dd283e0ba1d78a8e5a8dcde76d7d4
desch296 <me@dennisschmidt.net>
2016-10-05 17:29:01 +0200

    integrated adding to numbers

--- Test.java
+++ Test.java
@@ -1,12 +1,15 @@
 import java.util.*;
-
 public class Test {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-
 		System.out.println("Bitte eingeben [+-*/] zahl1 zahl2:");
 		String operator = scanner.next();
 		int zahl1 = scanner.nextInt();
 		int zahl2 = scanner.nextInt();
+		int result = 0;
+		if(operator.equals('+')) {
+			result = zahl1 + zahl2;
+		}
+		System.out.println(zahl1 + " " + operator + " " + zahl2 + " = " + result);
 	}
 }
